614baf4ee02fa91f37c059caba8f22cbad8154b2
Nicolas Goux <nicolasgoux2000@gmail.com>
2023-01-30 16:13:15 -0300

    solucionado bug con la imagen de fondo

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import {BaseLayout} from './Containers/BaseLayout';
 
 function App() {
 	return (
-		<div
-			className='flex items-center justify-center w-full h-screen bg-[#fed6e3] bg-cover'
-			style={{backgroundImage: `url(${'/background-travel-2.jpg'})`}}
-		>
+		<div className='background-plane flex items-center justify-center w-full h-screen bg-[#fed6e3] bg-cover'>
 			<BaseLayout />
 		</div>
 	);
